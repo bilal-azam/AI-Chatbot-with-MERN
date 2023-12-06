5d5e28056bc7f630358ec4cfc2540ad11ccab315
Bilal Azam <bilal.azam1@outlook.com>
2023-12-06 00:00:00 +0000

    Create Redux reducers and actions

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,9 @@
+// client/src/store/reducers/index.js
+import { combineReducers } from 'redux';
+// Import reducers here
+
+const rootReducer = combineReducers({
+    // Add reducers here
+});
+
+export default rootReducer;
