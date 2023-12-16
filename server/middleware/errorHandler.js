f574ad0197109b71333ec72f5636d7d7f0a4f385
Bilal Azam <bilal.azam1@outlook.com>
2023-12-16 00:00:00 +0000

    Add backend logging and error handling

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+// server/middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+};
+
+module.exports = errorHandler;
