1b9d117cbf84016eb79cf05fbc5299e326fca0f1
Bilal Azam <bilal.azam1@outlook.com>
2023-12-19 00:00:00 +0000

    Secure routes with authentication

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,12 +1,7 @@
 // server/routes/chat.js
-const express = require('express');
-const router = express.Router();
+const auth = require('../middleware/auth');
 
-// Sample endpoint
-router.post('/message', (req, res) => {
+router.post('/message', auth, (req, res) => {
     const { message } = req.body;
-    // Here you would handle the message, possibly interacting with a model or service
     res.json({ response: 'Message received' });
 });
-
-module.exports = router;
