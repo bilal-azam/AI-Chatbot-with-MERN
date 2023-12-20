e4effb6238207567dc252911a94be0cd8b289352
Bilal Azam <bilal.azam1@outlook.com>
2023-12-20 00:00:00 +0000

    Add user model and registration route

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
