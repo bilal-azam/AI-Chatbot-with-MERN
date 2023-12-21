da15a37d60ea9292a824de42622b006c2eb5838c
Bilal Azam <bilal.azam1@outlook.com>
2023-12-21 00:00:00 +0000

    Add user registration route

---
+++ server/routes/auth.js
@@ -0,0 +1,42 @@
+// server/routes/auth.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Register route
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        // Check if user exists
+        let user = await User.findOne({ email });
+        if (user) return res.status(400).json({ msg: 'User already exists' });
+
+        user = new User({ name, email, password });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Return jsonwebtoken
+        const payload = { user: { id: user.id } };
+        jwt.sign(
+            payload,
+            'your_jwt_secret',
+            { expiresIn: 3600 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
