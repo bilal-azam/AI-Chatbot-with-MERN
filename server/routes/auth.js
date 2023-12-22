0eefe6ca079c31164a05af959b89ecd64637902a
Bilal Azam <bilal.azam1@outlook.com>
2023-12-22 00:00:00 +0000

    Implement login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,28 +1,15 @@
 // server/routes/auth.js
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-// Register route
-router.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+// Login route
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
     try {
-        // Check if user exists
         let user = await User.findOne({ email });
-        if (user) return res.status(400).json({ msg: 'User already exists' });
+        if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
 
-        user = new User({ name, email, password });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
-        // Encrypt password
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-        await user.save();
-
-        // Return jsonwebtoken
         const payload = { user: { id: user.id } };
         jwt.sign(
             payload,
@@ -38,5 +25,3 @@ router.post('/register', async (req, res) => {
         res.status(500).send('Server error');
     }
 });
-
-module.exports = router;
