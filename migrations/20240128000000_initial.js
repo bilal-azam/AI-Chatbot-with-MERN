872badbe32c1aecb726ad624e91fe279fdf7712f
Bilal Azam <bilal.azam1@outlook.com>
2024-01-28 00:00:00 +0000

    Add migration scripts directory and example migration

---
+++ migrations/20240128000000_initial.js
@@ -0,0 +1,11 @@
+// migrations/20240128000000_initial.js
+module.exports = {
+    up: async () => {
+        // Example migration script
+        console.log('Running initial migration');
+    },
+    down: async () => {
+        // Rollback script
+        console.log('Rolling back initial migration');
+    }
+};
