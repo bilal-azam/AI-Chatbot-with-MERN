eb16b3a93048499c55e2178da9a451ce04729faf
Bilal Azam <bilal.azam1@outlook.com>
2024-04-08 00:00:00 +0100

    Update frontend to include logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ErrorBoundary from './components/ErrorBoundary';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 import ItemList from './components/ItemList';
+import Logout from './components/Logout';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => (
     <ErrorBoundary>
         <div>
             <h1>MyApp</h1>
-            <ItemList />
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/items" component={ItemList} />
+                <Route path="/logout" component={Logout} />
+                <Route path="/" component={Login} />
+            </Switch>
         </div>
     </ErrorBoundary>
 );
