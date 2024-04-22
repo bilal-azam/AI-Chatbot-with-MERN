96273e67fb8c47c68b3af8e7b2cd942945ae3bc1
Bilal Azam <bilal.azam1@outlook.com>
2024-04-23 00:00:00 +0100

    Perform final review and testing of deployment scripts

--- scripts/deploy-eb.sh
+++ scripts/deploy-eb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # scripts/deploy-eb.sh
 
-aws elasticbeanstalk create-application-version     --application-name my-app     --version-label v1.0.0     --source-bundle S3Bucket=my-bucket,S3Key=my-app-v1.0.0.zip
-
-aws elasticbeanstalk update-environment     --application-name my-app     --environment-name my-app-env     --version-label v1.0.0
+# Deploy to Elastic Beanstalk
+aws elasticbeanstalk create-application-version --application-name my-app --version-label `date +%Y%m%d%H%M%S` --source-bundle S3Bucket=my-app-bucket,S3Key=my-app.zip
+aws elasticbeanstalk update-environment --environment-name my-app-env --version-label `date +%Y%m%d%H%M%S`
